bd35c97cc3a4dc371f300b83b0e51639e1ea86b4
Moustafa Nady <mostafanadii93@gmail.com>
2024-02-26 14:24:45 +0100

    Creating and styling Login Form with Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import RegisterPage from './components/RegisterPage';
 import LandingPage from './components/LandingPage';
+import LoginForm from './components/loginPage';
 import { Route , Routes } from 'react-router';
 import './styles/App.css';
 import './styles/ColourPalette.css';
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginForm />} />
       </Routes>
     </>
   )
---
+++ src/components/loginPage.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import '../styles/LoginPage.css';
+import { useNavigate } from 'react-router';
+
+
+const LoginForm = () => {
+
+    const navigate = useNavigate();
+
+    const { register, handleSubmit } = useForm();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+
+  const onSubmit = data => {
+    console.log(data);
+    // Here you would typically send the data to your server
+  };
+
+  const handleDiscordLogin = () => {
+    console.log('Discord login clicked');
+    // Here you would typically send the data to your server
+  };
+
+  const handleLoginClick = (path) => {
+    console.log(path);
+    navigate('/login');
+};
+
+return (
+    <div className='LoginContainer'>
+                <img className='landingImg' src='https://cdn.discordapp.com/attachments/1208068435725262938/1208070511322603551/POGLogo.png?ex=65eb2cd6&is=65d8b7d6&hm=fc26c274366064190fcbc3ffb82ef1538b3a7bfe73e4f71e3609aa83f7d96a3e&' alt='POG Logo' />
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label>
+                Username:
+                <input type="text" value={username} onChange={e => setUsername(e.target.value)} required />
+            </label>
+
+            <label>
+                Password:
+                <input type="password" value={password} onChange={e => setPassword(e.target.value)} required />
+            </label>
+
+            <div className='loginButtonsWrapper'>
+            <button className="discordButton" onClick={handleDiscordLogin}>Login with Discord</button>
+            <button className='registerButton' onClick={handleLoginClick}>Sign In</button>
+            <h1></h1>
+
+            </div>
+
+        </form>
+    </div>
+);
+};
+
+export default LoginForm;
