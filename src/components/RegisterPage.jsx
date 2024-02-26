03b543613a7334a93f686885c3dc032f7a1bacf2
Moustafa Nady <mostafanadii93@gmail.com>
2024-02-26 14:45:55 +0100

    styinling updates

--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -28,6 +28,7 @@ import { useState } from 'react';
       <div className='registerContainer'>
         <img className='landingImg' src='https://cdn.discordapp.com/attachments/1208068435725262938/1208070511322603551/POGLogo.png?ex=65eb2cd6&is=65d8b7d6&hm=fc26c274366064190fcbc3ffb82ef1538b3a7bfe73e4f71e3609aa83f7d96a3e&' alt='POG Logo' />
       <form onSubmit={handleSubmit}>
+      <div className='RegisterInput'>
         <label>
           Username:
           <input type="text" value={username} onChange={e => setUsername(e.target.value)} required />
@@ -42,6 +43,7 @@ import { useState } from 'react';
           Email:
           <input type="email" value={email} onChange={e => setEmail(e.target.value)} required />
         </label>
+        </div>
 
         <div className='landingButtonsWrapper'>
         <button className="discordButton" onClick={handleDiscordRegister}>Register with Discord</button>
--- src/components/loginPage.jsx
+++ src/components/loginPage.jsx
@@ -33,20 +33,23 @@ return (
     <div className='LoginContainer'>
                 <img className='landingImg' src='https://cdn.discordapp.com/attachments/1208068435725262938/1208070511322603551/POGLogo.png?ex=65eb2cd6&is=65d8b7d6&hm=fc26c274366064190fcbc3ffb82ef1538b3a7bfe73e4f71e3609aa83f7d96a3e&' alt='POG Logo' />
         <form onSubmit={handleSubmit(onSubmit)}>
+          
+        <div className='LoginInput'>
             <label>
                 Username:
                 <input type="text" value={username} onChange={e => setUsername(e.target.value)} required />
             </label>
+   
 
             <label>
                 Password:
                 <input type="password" value={password} onChange={e => setPassword(e.target.value)} required />
             </label>
+        </div>
 
             <div className='loginButtonsWrapper'>
             <button className="discordButton" onClick={handleDiscordLogin}>Login with Discord</button>
-            <button className='registerButton' onClick={handleLoginClick}>Sign In</button>
-            <h1></h1>
+            <button className='LoginButton' onClick={handleLoginClick}>Sign In</button>
 
             </div>
 
