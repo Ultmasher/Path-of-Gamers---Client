b73c7ad7df18543811882ca63bebba5c0be1c066
CiarJ <cjones.de117@gmail.com>
2024-02-26 12:05:07 +0100

    Adjusted File Structure to add new Landing Page Header. Routing now leads to Landing Page as default and can route to /register from there. Vite CSS Styling files have also been removed and a Colour Variable CSS Palette has also been added into /styles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import RegisterForm from './components/RegisterForm'
-import { Route , Routes } from 'react-router'
-import './App.css'
+import RegisterForm from './components/RegisterForm';
+import LandingPage from './components/LandingPage';
+import { Route , Routes } from 'react-router';
+import './styles/App.css';
+import './styles/ColourPalette.css';
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<RegisterForm />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/register" element={<RegisterForm />} />
       </Routes>
     </>
   )
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useNavigate } from 'react-router';
+import '../styles/LandingPage.css';
+
+const LandingPage = () => {
+
+  const navigate = useNavigate();
+
+  const handleSignInClick = (path) => {
+    console.log(path);
+    navigate('/login');
+  };
+
+  const handleRegisterClick = (path) => {
+    console.log(path);
+    navigate('/register');
+  };
+
+  return (
+    <>
+        <div className='landingContainer'>
+            <img className='landingImg' src='https://cdn.discordapp.com/attachments/1208068435725262938/1208070511322603551/POGLogo.png?ex=65eb2cd6&is=65d8b7d6&hm=fc26c274366064190fcbc3ffb82ef1538b3a7bfe73e4f71e3609aa83f7d96a3e&' alt='POG Logo' />
+            <div className='landingButtonsWrapper'>
+                <button className='landingButton' onClick={handleSignInClick}>SIGN IN</button>
+                <button className='landingButton' onClick={handleRegisterClick}>CREATE ACCOUNT</button>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default LandingPage
