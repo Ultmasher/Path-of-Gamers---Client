5e2e262e864355bba7831ececd71004d920990fe
Moustafa Nady <mostafanadii93@gmail.com>
2024-03-01 10:25:43 +0100

    fixing upload

--- src/components/AccountSettings.jsx
+++ src/components/AccountSettings.jsx
@@ -53,8 +53,9 @@ const AccountSettings = () => {
                 <div className='accountSettingsFormRight'>
                     <label htmlFor='region'>Region:</label>
                     <select className='regionSelect' id='region' name='region'>
-                        <option value='na'>EU: West</option>
-                        <option value='eu'>EU: East</option>
+                        <option value='na'>North America</option>
+                        <option value='eu'>Europe</option>
+                        <option value='asia'>Asia</option>
                     </select>
 
                     <label htmlFor='firstName'>First Name:</label>
