b2d266c5317547161601de95b38745281163796c
Antonio Bjelobrk <bjelobrka@gmail.com>
2024-03-08 11:08:01 +0100

    loaded the posts from user

--- src/components/HomeFeed.jsx
+++ src/components/HomeFeed.jsx
@@ -5,6 +5,7 @@ import '../styles/HomeFeed.css';
 import { useAuth } from '../context/AuthContext';
 
 const HomeFeed = () => {
+  const [loading, setLoading] = useState(true);
   const [postImage, setPostImage] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
   const [posts, setPosts] = useState([]);
@@ -23,6 +24,16 @@ const HomeFeed = () => {
       })
       .catch(error => {
         console.error('There was a problem with the fetch operation:', error);
+      });
+
+    axios.get('http://localhost:8000/post')
+      .then(response => {
+        setPosts(response.data);
+        setLoading(false); // Set loading to false once data is fetched
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+        setLoading(false); // Set loading to false in case of error
       });
   }, []);
 
@@ -43,7 +54,8 @@ const HomeFeed = () => {
     newPostData.append('content', postData.content);
     newPostData.append('gameId', selectedGame);
     newPostData.append('userId', user._id);
-    newPostData.append('image', postImage); // No need to convert to base64
+    newPostData.append('picture', postImage); // No need to convert to base64
+
 
     try {
       const response = await axios.post('http://localhost:8000/post', newPostData, {
@@ -62,12 +74,13 @@ const HomeFeed = () => {
     }
   };
 
+
   return (
     <div>
-      <form onSubmit={handlePostSubmit} className='eventInputsWrapper'>
+      <form onSubmit={handlePostSubmit} className='eventPostWrapper'>
         <label htmlFor="bio">Create a post:</label>
         <textarea
-          rows="8"
+          rows="1"
           cols="100"
           maxLength="150"
           id='bio'
@@ -88,12 +101,15 @@ const HomeFeed = () => {
             <option key={game._id} value={game._id}>{game.name}</option>
           ))}
         </select>
-        <div htmlFor="profile-picture">
+        <div htmlFor="profile-picture" >
+          <label htmlFor="profile-picture" className="custom-file-upload">
+            Upload Picture
+          </label>
           <input
             type="file"
             id="profile-picture"
-            name="profile-picture"
-            accept="image/png, image/jpeg"
+            name="picture"
+            accept="image/*"
             onChange={handlePostImageChange}
           />
         </div>
@@ -101,6 +117,15 @@ const HomeFeed = () => {
           <button className='changeAvatarButton'>Post</button>
         </div>
       </form>
+      <div className='postsWrap'>
+        {posts.map((post, index) => (
+          <div key={index}>
+            <p>ID: {post._id}</p>
+            <p>Content: {post.content}</p>
+            <p>Type: {post.type}</p>
+          </div>
+        ))}
+      </div>
       <Modal className='Modaltext' open={isOpen} onClose={() => setIsOpen(false)}>
         Your post has been posted!
       </Modal>
