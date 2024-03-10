92f996ffccb7974e49cfc37936dd78f7f3139920
Moustafa Nady <mostafanadii93@gmail.com>
2024-03-10 23:02:53 +0100

    follow

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -2,47 +2,12 @@ import React from 'react';
 import '../styles/ProfilePage.css';
 import ProfileTabMenu from './ProfileTabMenu';
 import { useAuth } from '../context/AuthContext';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 
 const ProfilePage = () => {
 
     const { user } = useAuth();
     console.log(user)
 
-    const { currentUser } = useAuth();
-    const [setUser] = useState(null);
-
-    useEffect(() => {
-      const fetchUser = async () => {
-        try {
-          const response = await axios.get(`http://localhost:8000/user/${currentUser.id}`);
-          setUser(response.data);
-        } catch (error) {
-          console.error('Error fetching user:', error);
-        }
-      };
-  
-      fetchUser();
-    }, [currentUser.id]);
-
-    const followUser = async (userId) => {
-        try {
-            await axios.post(`http://localhost:8000/follow/${userId}`, { followerId: currentUser.id });
-            // Update UI accordingly
-          } catch (error) {
-            console.error('Error following user:', error);
-          }
-        };
-
-        const unfollowUser = async (userId) => {
-            try {
-                await axios.post(`http://localhost:8000/unfollow/${userId}`, { followerId: currentUser.id });
-                // Update UI accordingly
-              } catch (error) {
-                console.error('Error unfollowing user:', error);
-              }
-            }
 
     return (
         <div className='profilePageContainer'>
