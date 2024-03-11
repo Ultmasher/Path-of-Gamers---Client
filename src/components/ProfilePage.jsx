fa21ee1ec405eedb307ee82ddc31d2c95b9f0e59
Moustafa Nady <mostafanadii93@gmail.com>
2024-03-11 16:51:02 +0100

    follow

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -2,32 +2,11 @@ import React, { useState } from 'react';
 import '../styles/ProfilePage.css';
 import ProfileTabMenu from './ProfileTabMenu';
 import { useAuth } from '../context/AuthContext';
-import axios from 'axios';
 
 const ProfilePage = () => {
   const { currentUser } = useAuth();
-  const [user, setUser] = useState(null);
-  const [isFollowing, setIsFollowing] = useState(false);
-
   console.log(currentUser);
 
-  const followUser = async (userId) => {
-    try {
-      await axios.post(`http://localhost:8000/follow/${userId}`, { followerId: currentUser.id });
-      // Update UI accordingly
-    } catch (error) {
-      console.error('Error following user:', error);
-    }
-  };
-
-  const unfollowUser = async (userId) => {
-    try {
-      await axios.post(`http://localhost:8000/unfollow/${userId}`, { followerId: currentUser.id });
-      // Update UI accordingly
-    } catch (error) {
-      console.error('Error unfollowing user:', error);
-    }
-  };
 
   return (
     <div className='profilePageContainer'>
